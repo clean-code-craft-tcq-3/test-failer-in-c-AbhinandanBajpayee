73f80a08938c74436d26b5b5a8a1faecb1614f6b
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-04-28 15:28:57 +0530

    Update CommonHeader.h

--- CommonHeader.h
+++ CommonHeader.h
@@ -5,3 +5,10 @@ char size(int cms);
 int networkAlertStub(float celcius);
 void alertInCelcius(float farenheit);
 float farenheitToCelcius(float farenheit);
+
+struct Colorpair
+{
+	char MajorColor[10];
+	char MinorColor[10];
+};
+
