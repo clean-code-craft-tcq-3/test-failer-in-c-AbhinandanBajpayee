abc1aaf7a705005b75ccc4236ece239d2efa2f4c
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-04-28 15:31:24 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -2,12 +2,15 @@
 #include <assert.h>
 #include "CommonHeader.h"
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
+olorpair colorpair[25];
+
+int printAndUpdateColorData() {
+    int i = 0, j = 0, localPairNumber=0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
+            localPairNumber = localPairNumber+1;
+            strcpy(colorpair[localPairNumber].MajorColor , majorColor[i]);
+	        strcpy(colorpair[localPairNumber].MinorColor , minorColor[i]);
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
@@ -15,8 +18,10 @@ int printColorMap() {
 }
 
 int main() {
-    int result = printColorMap();
+    int result = printAndUpdateColorData();
     assert(result == 25);
+    assert(strcmp(colorpair[23].MajorColor, "Violet") == 0);
+	assert(strcmp(colorpair[23].MinorColor, "Green") == 0);
     printf("All is well (maybe!)\n");
     return 0;
 }
